ab86a3312de59368adea9e79d90a835d9259a23f
darlynmendez <77795140+darlynmendez@users.noreply.github.com>
2021-04-21 12:46:19 -0400

    Assignment04

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace connect4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connect4MenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connect4MenuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connect4MenuToolStripMenuItem
+            // 
+            this.connect4MenuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.quitToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.connect4MenuToolStripMenuItem.Name = "connect4MenuToolStripMenuItem";
+            this.connect4MenuToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.connect4MenuToolStripMenuItem.Text = "Connect4 Menu";
+            this.connect4MenuToolStripMenuItem.Click += new System.EventHandler(this.connect4MenuToolStripMenuItem_Click);
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connect4MenuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿// Name: Darlyn Mendez
+// CSC339 - Spring 2021
+// Assignment 4
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace connect4
+{
+    public partial class Form1 : Form
+    {
+        private Board board;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Adds the event handler for when the screen is painted
+            this.Paint += new PaintEventHandler(pic_board_Paint);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Gets called when the main form is loaded
+            board = new Board();
+        }
+
+        private void connect4MenuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pic_board_Paint(object sender, PaintEventArgs e)
+        {
+            // Gets called whenever the screen is painted
+            board.DrawBoard(e.Graphics);
+        }
+
+        private void Form1_MousClick(object sender, MouseEventArgs e)
+        {
+            int colsIndex = this.ColsNumber(e.Location);
+            if (colsIndex != -1)
+            {
+                int indexRows = this.EmptyRow(colsIndex);
+                if (indexRows != -1)
+                {
+                    this.board[indexRows, colsIndex] = this.turn;
+                    if (this.turn == 1)
+                    {
+                        Graphics g == this.CreateGraphics();
+                        g.FillEllipse(Brushes.Red, 32 + 48 * colsIndex, 32 + 48 * indexRows, 32, 32);
+                    }
+                    else if (this.turn == 2)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        g.FillEllipse(Brushes.Yellow, 32 + 48 * colsIndex, 32 + 48 * indexRows, 32, 32);
+                    }
+                }
+            }
+        }
+
+        private int ColsNumber(Point mouse)
+        {
+            for (int i = 0; i < this.board.Length; i++)
+            {
+                if ((mouse.X >= this.board[i].X) && (mouse.Y >= this.board[i].Y))
+                {
+                    if((mouse.X <= this.board[i].X + this.board[i].Width)&&(mouse.Y <= this.board[i].Y+this.board[i].Height))
+                    {
+                        return i;
+                    }
+                }
+        }
+            return -1;
+    }
+
+
+}
